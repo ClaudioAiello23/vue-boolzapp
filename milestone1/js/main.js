1f696769551eaae60035b5ddae11dd0918e3b367
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-17 10:37:38 +0100

    sistemazione img errata

--- milestone1/js/main.js
+++ milestone1/js/main.js
@@ -113,7 +113,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.jpg',
+                    avatar: './img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
